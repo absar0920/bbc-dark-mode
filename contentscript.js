47674fc9aaf0db4a2149c50d1eac1dcd983f2c2f
Absar <absar0920@proton.me>
2024-02-11 11:12:47 +0500

    Testing the extension

---
+++ contentscript.js
@@ -0,0 +1,12 @@
+function createDarkMode(){
+    let button = document.createElement("button");
+    button.id = "bbc-dark-mode-button";
+    button.innerHTML = "Toggle Dark Mode"
+    button.addEventListener("click", toggleDarkMode)
+    document.body.prepend(button)
+}
+
+function toggleDarkMode(){
+    document.body.classList.toggle("bbc-dark-mode")
+    console.log('Dark Mode toggled');
+}
